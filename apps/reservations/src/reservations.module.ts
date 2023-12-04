b974f22694003c472afbd755a33a834bb32bef4a
DieselNiu <niu853273494@qq.com>
2023-12-04 20:59:56 +0800

    users && password

--- apps/reservations/src/reservations.module.ts
+++ apps/reservations/src/reservations.module.ts
@@ -2,6 +2,10 @@ import { DatabaseModule } from '@app/common';
 import { LoggerModule } from '@app/common/logger';
 import { Module } from '@nestjs/common';
 
+import { ConfigModule } from '@nestjs/config';
+
+import * as Joi from 'joi';
+
 import { ReservationDocument, ReservationSchema } from './models/reservation.schema';
 import { ReservationRepistory } from './reservation.repository';
 import { ReservationsController } from './reservations.controller';
@@ -12,6 +16,13 @@ import { ReservationsService } from './reservations.service';
         DatabaseModule,
         DatabaseModule.forFeature([{ name: ReservationDocument.name, schema: ReservationSchema }]),
         LoggerModule,
+        ConfigModule.forRoot({
+            isGlobal: true,
+            validationSchema: Joi.object({
+                MONGODB_URI: Joi.string().required(),
+                PORT: Joi.number().required(), 
+            }),
+        }),
     ],
     controllers: [ReservationsController],
     providers: [ReservationsService, ReservationRepistory],
